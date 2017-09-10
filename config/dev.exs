592f8e1d0f99c839208a14a1a78d6422caecaefc
Kyle Bustard <bustardkyle@gmail.com>
2017-09-09 21:24:26 -0500

    create user controller, view & template

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :hello, Hello.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "kylebustard",
+  password: "monkeebizniz",
   database: "hello_dev",
   hostname: "localhost",
   pool_size: 10
